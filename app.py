33c9f13c0a83c1b2fad48c051360c91abaebea92
Pelin Balcı <balci.pelin@gmail.com>
2022-12-04 22:55:58 +0300

    reformat

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from train_predict_page import show_train_predict_page
 from explore_page import show_explore_page
 from references_page import show_references
 
-page = st.sidebar.selectbox("Pages", ("Predict", "Explore", "References"))
+page = st.sidebar.selectbox("Pages", ("Recommendation", "Explore", "References"))
 
-if page == "Predict":
+if page == "Recommendation":
     show_train_predict_page()
 elif page == "Explore":
     show_explore_page()
