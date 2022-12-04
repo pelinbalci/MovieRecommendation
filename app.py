ac6ab12864a038e9160f20a8f9de981a97a378ff
Pelin Balcı <balci.pelin@gmail.com>
2022-12-04 22:08:04 +0300

    pages

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ The original source: https://github.com/patrickloeber/ml-app-salaryprediction
 
 import streamlit as st
 from train_predict_page import show_train_predict_page
-# from explore_page import show_explore_page
+from explore_page import show_explore_page
+from references_page import show_references
 
-
-page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+page = st.sidebar.selectbox("Pages", ("Predict", "References", "Explore"))
 
 if page == "Predict":
     show_train_predict_page()
-# else:
-#     show_explore_page()
+elif page == "Explore":
+    show_explore_page()
+else:
+    show_references()
