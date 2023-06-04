963bb0489d577fbd25f136774836b39859277e3f
Pelin Balcı <balci.pelin@gmail.com>
2023-06-04 14:36:46 +0300

    new page

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import streamlit as st
-from train_predict_page import show_train_predict_page
+from train_predict_page_v0 import show_train_predict_page_v0
 from explore_page import show_explore_page
 from references_page import show_references
 from tuning_page import show_tuning_page
+from train_predict_page_v1 import show_train_predict_page_v1
 
-page = st.sidebar.selectbox("Pages", ("Recommendation", "Tune the Model", "Explore", "References"))
+page = st.sidebar.selectbox("Pages", ("Recommendation_v0", "Recommendation_v1", "Tune the Model", "Explore", "References"))
 
-if page == "Recommendation":
-    show_train_predict_page()
+if page == "Recommendation_v0":
+    show_train_predict_page_v0()
+
+elif page == "Recommendation_v1":
+    show_train_predict_page_v1()
 
 elif page == "Tune the Model":
     show_tuning_page()
